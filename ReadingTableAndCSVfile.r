31b14de4a53fa32185dec20ac9c171bebe709ed3
inam awan <inamulhassan1124@gmail.com>
2021-02-20 09:59:42 +0500

    Added and updated more lines..

--- ReadingTableAndCSVfile.r
+++ ReadingTableAndCSVfile.r
@@ -1,2 +1,13 @@
 #This is the link of world bank data GDP
 FileUrl<-"http://databank.worldbank.org/data/download/GDP.csv"
+download.file(FileUrl,destfile = "../ReadingText-CSVFiles/GDPData.csv")
+list.files("../ReadingText-CSVFiles/GDPData.csv")
+DatDGP<-read.csv("../ReadingText-CSVFiles/GDPData.csv")
+#structure...
+str(DatDGP)
+DateDownloaded<-date()
+#show the date of Downloaded
+DateDownloaded
+#Reading the table
+CdataGDP<-read.table("GDPData.csv",header = TRUE,fill = TRUE)
+View(CdataGDP)
